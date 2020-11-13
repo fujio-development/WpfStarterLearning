f2252d68c1463729c8a5035bfc8ef6b5f802cd80
local-fujio <treasure.ship005@gmail.com>
2020-11-13 20:42:26 +0900

    Win201113

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ### 環境
 
 ```
-開発環境：Microsoft Visual Studio 2019 Community Edition 16.7.7  
+開発環境：Microsoft Visual Studio 2019 Community Edition 16.8.1  
 言語：C#.NET 8.0  
 デスクトップ：WPFアプリケーション
-デベロッパーSDK：.NET Core SDK 3.1.403  
+デベロッパーSDK：.NET Core SDK 5.0.100  
 データベース：Microsoft SQLServer 2019 Express CU8  
 -データベース管理ツール：Microsoft SQL Server Management Studio 18.7.1  
 データベース接続：.NET Framework Data Provider for SQL Server  
--- WpfStarterLearning/obj/Release/netcoreapp3.1/WindowTimer.g.i.cs
+++ WpfStarterLearning/obj/Release/netcoreapp3.1/WindowTimer.g.i.cs
@@ -151,13 +151,13 @@ namespace WpfStarterLearning {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfStarterLearning;component/windowtimer.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfStarterLearning;V1.0.0.0;component/windowtimer.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\WindowTimer.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -167,7 +167,7 @@ namespace WpfStarterLearning {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
